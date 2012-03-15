af5eb44cff273cd5ac18b859a3e8a7a2b691bb25
Shea Clare <sheaclare@host39-213.wifi.ubc.ca>
2012-03-14 18:39:20 -0700

    Ridge Regression vs. Lasso Visualizations

---
+++ Ridge_Regression_vs_Lasso/README.txt
@@ -0,0 +1,13 @@
+The script in a5_comb.py calls functions in a5_util to compare the performance of Lasso and Ridge Regression.
+
+Specifically it:
+1) Plots the raw data as a scatterplot
+2) Graphs the regularization paths against different degrees of freedom for the Ridge method
+3) Graphs the squared errors against different degrees of freedom for the Ridge method
+4) Does the same for Lasso method, using the Shooting algorithm
+
+I guess it may be redundant to say, but to run this puppy, just run the driver class a5_comb.py.
+It may not be redundant to mention that the functions found in a5_util are probably useful outside of this specific context. 
+Feel free to use them, but please cite me (Shealen Clare) and the class that asked me to figure out and then construct this stuff for homework: (UBC Computer Science 340: Machine Learning).
+
+Thanks :)
---
+++ Ridge_Regression_vs_Lasso/a5_util.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as pl
+import numpy.linalg as la
+import numpy as np
+import types
+
+#################################################################
+
+def ridge(X_init, y_init, d2_vec):
+    X, y, _ = standardize(X_init, y_init)
+    theta = np.empty((len(d2_vec), np.shape(X)[1]))
+
+    for j,d2 in enumerate(d2_vec):
+        d2I = np.diag(d2 * np.ones( np.shape(X)[1] ))
+        XTX_plus_d2I = np.dot(X.T, X) + d2I
+        theta[j] = np.dot( np.dot( la.inv(XTX_plus_d2I), X.T ), y )
+
+    return theta
+
+#################################################################
+
+def standardize(X, y, opts=None):
+
+    if opts == None:
+        opts = {}
+        opts['xmean'] = np.mean(X, axis=0)
+        opts['ymean'] = np.mean(y)
+        opts['scale'] = np.std(X-opts['xmean'], axis=0)
+
+    zeros = np.empty(shape=np.shape(opts['scale']), dtype=bool)
+    zeros = opts['scale'] == 0
+    nonzeros = np.logical_not(zeros)
+
+    X_s = np.empty(np.shape(X))
+    X_s[:,nonzeros] = \
+        (X[:,nonzeros] \
+        - opts['xmean'][nonzeros]) \
+        / opts['scale'][nonzeros]
+    X_s[:,zeros] = 0
+    y_s = y - opts['ymean']
+    return X_s, y_s, opts
+
+#################################################################
+
+def deg_of_frdm(X_init, y_init, d2_vec):
+
+    ## need to standardize??
+    X, y, _ = standardize(X_init, y_init)
+    U, S, Vt = la.svd(X)
+
+    df = np.empty(len(d2_vec))
+    for j,d2 in enumerate(d2_vec):
+        df[j] = sum( [ s**2 / (s**2 + d2) for s in S])
+    return df
+
+#################################################################
+
+def sqrd_err(X, y, k, function, d2_vec):
+
+    n = np.shape(X)[0]
+    d = np.shape(X)[1]
+    d_round = 100 # cheating - this should not be magic number
+
+    errors = np.empty((k, d_round))
+
+    for i in xrange(k):
+        test = np.zeros(n, dtype=bool)
+        test[0:i*k] = True
+        start_test_again = min((i+1)*k, n-1)
+        test[start_test_again:] = True
+
+        train = np.logical_not(test)
+
+        Xtrain, ytrain, opts = standardize(X[train], y[train])
+        Xtest, ytest, _ = standardize(X[test], y[test], opts)
+        theta = function(Xtrain, ytrain, d2_vec)
+
+        for j in xrange(d_round):
+            errors[i, j] = np.mean((ytest - np.dot(Xtest, theta[j]))**2)
+
+    err_mean = np.mean(errors, axis=0)
+    err_std = np.std(errors, axis=0)
+    return err_mean, err_std
+
+#################################################################
+
+def lasso(X_init, y_init, d2_vec):
+
+    THRESH = 1e-5
+    X, y, opts = standardize(X_init, y_init)
+    n = np.shape(X)[0]
+    d = np.shape(X)[1]
+
+    theta = ridge(X, y, d2_vec)
+    theta_ = np.empty(np.shape(theta))
+
+    for i,d2 in enumerate(d2_vec):
+        theta_vec = theta[i]
+        theta_vec_ = np.zeros(np.shape(theta_vec))
+
+        while np.sum(np.abs(theta_vec - theta_vec_)) > THRESH:
+            #print 'sum of errs = ', np.sum(np.abs(theta_vec - theta_vec_))
+            theta_vec = theta_vec_
+            theta_vec_ = theta_vec.copy()
+
+            for j in range(d):
+                a_j = 2*np.sum( X[:,j]**2 )
+                c_j_vec = np.zeros((n,1))
+
+                for k in range(n):
+                    c_j_vec[k] = \
+                        X[k,j] * (y[k] \
+                        - np.dot(theta_vec_.T, X[k]) \
+                        + theta_vec_[j] + X[k,j])
+                c_j = np.sum(c_j_vec)
+
+                if c_j < -d2:
+                    theta_vec_[j] = (c_j + d2) / a_j
+                elif c_j > d2:
+                    theta_vec_[j] = (c_j - d2) / a_j
+                else:
+                    theta_vec_[j] = 0
+        theta[i] = theta_vec_
+    return theta
+
