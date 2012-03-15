bac8d6bf810cd8632fdcaf5cda5a5a0d02127ec8
Shea Clare <sheaclare@host220-214.wifi.ubc.ca>
2012-03-15 10:26:10 -0700

    file name changes

--- Ridge_Regression_vs_Lasso/README.txt
+++ Ridge_Regression_vs_Lasso/README.txt
@@ -1,4 +1,4 @@
-The script in a5_comb.py calls functions in a5_util to compare the performance of Lasso and Ridge Regression.
+The script in ridge_vs_lasso.py calls functions in util_functions.py to compare the performance of Lasso and Ridge Regression.
 
 Specifically it:
 1) Plots the raw data as a scatterplot
@@ -6,8 +6,8 @@ Specifically it:
 3) Uses 10-fold Cross Validation to graph the squared errors against different degrees of freedom for the Ridge method
 4) Does the same for Lasso method, using the Shooting algorithm
 
-I guess it may be redundant to say, but to run this puppy, just run the driver class a5_comb.py.
-It may not be redundant to mention that the functions found in a5_util are probably useful outside of this specific context. 
+I guess it may be redundant to say, but to run this puppy, just run the driver class ridge_vs_lasso.py.
+It may not be redundant to mention that the functions found in util_functions.py are probably useful outside of this specific context. 
 Feel free to use them, but please cite me (Shealen Clare) and the class that asked me to figure out and then construct this stuff for homework: (UBC Computer Science 340: Machine Learning).
 
 Thanks :)
---
+++ Ridge_Regression_vs_Lasso/ridge_vs_lasso.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy.linalg as la
+import numpy as np
+import types
+import util_functions as u_f
+
+X = np.loadtxt('prostate.data')
+d2_vec = np.logspace(-5, 10, num=100)
+X_ = X[:,:-1]
+y_ = X[:,-1]
+
+####### 1: Data Visualization #######
+
+plt.figure(1)
+for i  in range(np.shape(X)[1]):
+    for j in range(np.shape(X)[1]):
+        if i != j:
+            plt.subplot(9,9, 9*i + j + 1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.scatter(X[:,i], X[:,j])
+plt.grid()
+plt.show()
+
+###### 2.a: Regularization Path for Ridge  #######
+
+df = u_f.deg_of_frdm(X_, y_, d2_vec)
+theta = u_f.ridge(X_, y_, d2_vec)
+
+plt.figure(2)
+plt.plot(df, theta)
+plt.grid()
+plt.show()
+
+####### 2.b Cross Validation for Ridge ########
+
+err_mean, err_std = u_f.sqrd_err(X_, y_, 10, u_f.ridge, d2_vec)
+plt.figure(3)
+plt.errorbar(df, err_mean, yerr=err_std)
+plt.grid()
+plt.show()
+
+###### 3.a Regularization Path for Lasso ######
+
+theta = u_f.lasso(X_, y_, d2_vec)
+
+plt.figure(4)
+plt.plot(df, theta)
+plt.grid()
+plt.show()
+
+###### 3.b Cross Validation for Lasso ######
+
+err_mean, err_std = u_f.sqrd_err(X_, y_, 10, u_f.lasso, d2_vec)
+plt.figure(5)
+plt.errorbar(df, err_mean, yerr=err_std)
+plt.grid()
+plt.show()
